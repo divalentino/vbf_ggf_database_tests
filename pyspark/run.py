ebb6f0d21fac74b5c928c68500deb4bf479f34be
David Di Valentino <david.di.valentino@cern.ch>
2018-02-13 11:14:11 -0500

    Add significance scan

--- pyspark/run.py
+++ pyspark/run.py
@@ -18,9 +18,36 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.functions import col
 
+from pyspark.sql.functions import udf
+from pyspark.sql.types import FloatType
+
 # Plotting tools.
 import matplotlib.pyplot as plt
 import numpy as np
+
+# UDF for getting first element of vector in dataframe.
+firstelement=udf(lambda v:float(v[0]),FloatType())
+
+#Some relevant routines for significance calculations.
+def get_yield(test_data, cut) :
+    myyield = -999.0
+    row     = test_data.select("label","weight",firstelement("probability")).filter(firstelement("probability")<cut).groupBy("label").sum().collect()
+    if (len(row)>0) :
+        myyield = row[0][2]
+    return myyield
+
+def asimovsig(s,b) :
+    sig = 2*((s+b)*np.log((s+b)/b)-s)
+    return sig
+
+def sigval(s,b,useAsimov) :
+    sigval=0.
+    if (s+b>0) :
+        if (useAsimov) :
+            sigval = asimovsig(s,b)
+        else :
+            sigval = s / np.sqrt(s+b)
+    return sigval
 
 ################################################################################
 # Depending on whether we're running in pyspark
@@ -105,12 +132,28 @@ for elt in range(0,len(coll_ggf)) :
 
 # Superimpose histograms of the BDT probability
 # (analogous to BDT response?)
-bins = np.linspace(0,1,25);
+bins = np.linspace(0,1,15);
 
 plt.hist(gbt_scores_vbf, bins, alpha=0.5, label='VBF', color='blue', weights = gbt_weights_vbf)
 plt.hist(gbt_scores_ggf, bins, alpha=0.5, label='ggF', color='red', weights = gbt_weights_ggf)
 plt.legend(loc='upper right')
-plt.show()
+#plt.show()
 
 # Perform a scan over the possible probability range to derive e.g. the optimal
 # approximate statistical significance.
+score_cuts = np.linspace(0.0,1.0,20)
+sig_values = []
+
+for score_cut in score_cuts :
+    #Get the sum total of events passing a given score cut.
+    yield_ggf = get_yield(gbt_pred_ggf,score_cut)
+    yield_vbf = get_yield(gbt_pred_vbf,score_cut)
+    #Calculate the approximate significance.
+    sig_values.append(sigval(yield_vbf,yield_ggf,True))
+
+# Find index of the maximal significance value - this provides our optimal cut.
+best_index = np.argmax(sig_values)
+best_cut   = score_cuts[best_index]
+best_sig   = sig_values[best_index]
+
+print "Best significance of %5.3f at score cut %5.3f" % (best_sig,best_cut)
